63af5d3d2691049f925670b69720b94c2a83ac76
Tirtha <tirthasg@gmail.com>
2022-09-14 16:48:09 +0530

    assignment_11/question_01.c added to repository

---
+++ assignment_11/question_01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    
+    int rem = b % a;
+    while (rem != 0) {
+        b = a;
+        a = rem;
+
+        rem = b % a;
+    }
+
+    return a;
+}
+
+int main(void)
+{
+    int num1, num2;
+    printf("Enter two integers: ");
+    scanf("%d%d", &num1, &num2);
+
+    int lcm = num1 * (num2 / gcd(num1, num2));
+    printf("LCM of %d and %d is %d\n", num1, num2, lcm);
+
+    return 0;
+}
