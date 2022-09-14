dda0dd38b804aff45b14c7e6d7f653d1d00d3f1b
Tirtha <tirthasg@gmail.com>
2022-09-15 03:34:29 +0530

    assignment_11/question_07.c code logic updated

--- assignment_11/question_07.c
+++ assignment_11/question_07.c
@@ -5,25 +5,13 @@ void print_fibonacci(int N)
     if (N <= 0)
         return;
 
-    if (N == 1) {
-        printf("%d\n", 0);
-        return;
-    }
+    int first = 0, second = 1, next;
+    for (int i = 1; i <= N; i++) {
+        printf("%d ", first);
 
-    if (N == 2) {
-        printf("%d %d\n", 0, 1);
-        return;
-    }
-
-    printf("%d %d ", 0, 1);
-    int first = 0, second = 1, next, count = 3;
-    while (count <= N) {
         next = first + second;
-        printf("%d ", next);
-
         first = second;
         second = next;
-        count++;
     }
     printf("\n");
 }
